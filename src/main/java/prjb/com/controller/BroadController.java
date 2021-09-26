75a3ac5f079d4d19e1cde850dd049e95d1bc1b91
Administrator <Administrator@DESKTOP-FU0RR20>
2021-09-26 19:42:21 +0900

    다국어갱신 모든톰캣에서 실행되도록 처리

--- src/main/java/prjb/com/controller/BroadController.java
+++ src/main/java/prjb/com/controller/BroadController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,7 @@ import prjb.com.service.ComService;
 
 /**
  * 로드밸런싱 되지않고, 모든톰캣에서 실행되어야 하는 url
+ * url생성시 /sub/ url도 함께 생성하여 다른톰캣에서도 실행되도록 처리한다.
  * @author Administrator
  *
  */
@@ -32,7 +34,12 @@ public class  BroadController{
 	 * 다국어 갱신
 	 */
 	@RequestMapping(value = "/setMlg")
-	public @ResponseBody Map setMlg(HttpServletRequest request) throws Exception {
+	public @ResponseBody Map setMlg(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		response.sendRedirect("/broad/sub/setMlg");
+		return setMlgExec(request, response);
+	}
+	@RequestMapping(value = "/sub/setMlg")
+	public @ResponseBody Map setMlgExec(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		logger.info("URL is {}.", "[" + request.getRequestURI() + "]");
 		Map<String, Object> resultMap = new HashMap();
 		
