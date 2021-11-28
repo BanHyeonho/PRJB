cff02525dfd9941c6b1a06efaf20c4799f1a819f
Administrator <Administrator@DESKTOP-FU0RR20>
2021-11-29 01:15:59 +0900

    암호화수정

--- src/main/java/prjb/com/init/InitBean.java
+++ src/main/java/prjb/com/init/InitBean.java
@@ -1,6 +1,5 @@
 package prjb.com.init;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
@@ -18,9 +17,8 @@ import prjb.com.util.CryptoUtil;
 public class InitBean{
 
 	//암호화변수
-	public static CryptoUtil CryptoClass;
+	private static CryptoUtil CryptoClass;
 	private static String key;
-	private static String[] encryptList;
 	public static ArrayList<String> encryptArray;
 	
 	//다국어
@@ -30,11 +28,19 @@ public class InitBean{
 	@Autowired
 	ComService comService;
 	
+	@Value("#{commonConfig['cryptoClass']}")
+	private String className;
+	
+	@Value("#{commonConfig['encrytKey']}")
+	private String encrytKey;
+	
+	@Value("#{commonConfig['encrytList']}")
+	private String encrytList;
+	
     /**
      * 암호화 클래스 주입
      * @param className
      */
-    @Value("#{commonConfig['cryptoClass']}")
     private void setCryptoClass(String className) throws Exception{
     	
         if(className == null){
@@ -43,35 +49,25 @@ public class InitBean{
         Class<?> dyClass = Class.forName(className);
         Object obj = dyClass.newInstance();
         CryptoClass = (CryptoUtil)obj;
+        key = CryptoClass.setKey(encrytKey);
+        
+        if(encrytList == null) {
+        	encryptArray = new ArrayList<String>();
+		}else {
+			encryptArray = new ArrayList(Arrays.asList( encrytList.split(",")));	
+		}
+        
     }
     
-	/**
-	 * 16자리의 키값으로 초기화한다. -키값의 길이는 16자리보다 커야한다.
-	 * @throws UnsupportedEncodingException 
-	 * 
-	 */
-	@Value("#{commonConfig['encrytKey']}")
-	private void setKey(String p_key) throws UnsupportedEncodingException{
-		key = CryptoClass.setKey(p_key);
-    }
-
-	@Value("#{commonConfig['encrytList']}")
-	private void setList(String encrytList){
-		if(encrytList == null) {
-			encryptArray = new ArrayList<String>();
-		}else {
-			encryptList = encrytList.split(",");
-			encryptArray = new ArrayList(Arrays.asList(encryptList));	
-		}
-    }
-	
 	/**
 	 * 의존성 주입후 초기화
 	 * @throws Exception
 	 */
 	@PostConstruct
     private void init() throws Exception {
-			
+		//암호화클래스
+		setCryptoClass(className);
+		
 		//다국어전체 초기화
 		comService.setMlg();
     }
--- src/main/java/prjb/com/util/CryptoUtil.java
+++ src/main/java/prjb/com/util/CryptoUtil.java
@@ -3,7 +3,7 @@ package prjb.com.util;
 import java.io.UnsupportedEncodingException;
 
 public interface CryptoUtil {
-	
+		
 	public String setKey(String p_key) throws UnsupportedEncodingException;
 	
 	public static String encrypt(String str) {
@@ -12,5 +12,4 @@ public interface CryptoUtil {
 	public static String decrypt(String str) {
 		return null;
 	}
-	
 }
