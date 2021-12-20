6bf0fe0948b854abd60b89c72a9820f8e0fe5f35
Administrator <Administrator@DESKTOP-FU0RR20>
2021-12-21 01:30:25 +0900

    오류수정

--- src/main/java/prjb/com/util/LinuxUtil.java
+++ src/main/java/prjb/com/util/LinuxUtil.java
@@ -1,9 +1,12 @@
 package prjb.com.util;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,19 +14,86 @@ public class LinuxUtil {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LinuxUtil.class);
 	
-	public static void shellCmd(String command) throws Exception {
+	public static boolean shellCmd(String command){
+		boolean result = false;
 		
-		logger.info(command);
-		
+		Process process = null;
         Runtime runtime = Runtime.getRuntime();
-        Process process = runtime.exec(command);
-        InputStream is = process.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        String line;
-        while((line = br.readLine()) != null) {
-           logger.info(line);
+        StringBuffer successOutput = new StringBuffer(); // 성공 스트링 버퍼
+        StringBuffer errorOutput = new StringBuffer(); // 오류 스트링 버퍼
+        BufferedReader successBufferReader = null; // 성공 버퍼
+        BufferedReader errorBufferReader = null; // 오류 버퍼
+        String msg = null; // 메시지
+ 
+        List<String> cmdList = new ArrayList<String>();
+ 
+        // 운영체제 구분 (window, window 가 아니면 무조건 linux 로 판단)
+        if (System.getProperty("os.name").indexOf("Windows") > -1) {
+            cmdList.add("cmd");
+            cmdList.add("/c");
+        } else {
+            cmdList.add("/bin/sh");
+            cmdList.add("-c");
         }
+        // 명령어 셋팅
+        cmdList.add(command);
+        String[] array = cmdList.toArray(new String[cmdList.size()]);
+ 
+        try {
+ 
+            // 명령어 실행
+            process = runtime.exec(array);
+ 
+            // shell 실행이 정상 동작했을 경우
+            successBufferReader = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
+ 
+            while ((msg = successBufferReader.readLine()) != null) {
+                successOutput.append(msg + System.getProperty("line.separator"));
+            }
+ 
+            // shell 실행시 에러가 발생했을 경우
+            errorBufferReader = new BufferedReader(new InputStreamReader(process.getErrorStream(), "UTF-8"));
+            while ((msg = errorBufferReader.readLine()) != null) {
+                errorOutput.append(msg + System.getProperty("line.separator"));
+            }
+ 
+            // 프로세스의 수행이 끝날때까지 대기
+            process.waitFor();
+ 
+            // shell 실행이 정상 종료되었을 경우
+            if (process.exitValue() == 0) {
+            	result = true;
+                logger.info("성공", successOutput.toString());
+                
+            } 
+            // shell 실행이 비정상 종료되었을 경우
+            else if(StringUtils.isNotBlank(errorOutput.toString())) {
+
+            	result = false;
+                logger.info("오류", errorOutput.toString());
+            }
+            // shell 실행이 비정상 종료되었을 경우
+            else {
+            	result = false;
+                logger.info("비정상 종료", successOutput.toString());
+                
+            }
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                process.destroy();
+                if (successBufferReader != null) successBufferReader.close();
+                if (errorBufferReader != null) errorBufferReader.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        return result;
 	}
 	
 }
