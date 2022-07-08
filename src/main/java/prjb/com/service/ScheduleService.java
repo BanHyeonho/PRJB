1691978a061e9ffa184c79c740afd989660a9303
Administrator <Administrator@DESKTOP-FU0RR20>
2022-07-09 03:37:54 +0900

    쓰레드 설정 수정

--- src/main/java/prjb/com/service/ScheduleService.java
+++ src/main/java/prjb/com/service/ScheduleService.java
@@ -36,7 +36,7 @@ public class ScheduleService {
 	 * @throws Exception
 	 */
 	//매일 오전1시에 실행(초 분 시 일 월 요일)
-	@Scheduled(cron = "0 20 1 * * *")
+	@Scheduled(cron = "0 0 1 * * *")
 	@SchedulerLock(name = "fileConvert", lockAtLeastFor = "23h", lockAtMostFor = "23h")
 	public void fileConvert() throws Exception {
 		logger.info("ScheduleService.fileConvert() START");
