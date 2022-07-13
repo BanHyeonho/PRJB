f6549fe06a691a58c26a766f3b2695ce2a3810fb
Administrator <Administrator@DESKTOP-FU0RR20>
2022-07-13 10:15:26 +0900

    쓰레드 수 수정

--- src/main/java/prjb/com/init/AsyncThreadConfiguration.java
+++ src/main/java/prjb/com/init/AsyncThreadConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class AsyncThreadConfiguration {
 
-	private final int CORE_POOL_SIZE = 50;					//동시 동작하는 최대 Thread 수
+	private final int CORE_POOL_SIZE = 10;					//동시 동작하는 최대 Thread 수
 	private final int MAX_POOL_SIZE = Integer.MAX_VALUE;	//쓰레드 풀의 최대사이즈
 	private final int QUEUE_CAPACITY = Integer.MAX_VALUE;	//MaxPoolSize 초과 요청에서 Thread 생성 요청시, 해당 요청을 Queue에 저장하는데 이때 최대 수용 가능한 Queue의 수, Queue에 저장되어있다가 Thread에 자리가 생기면 하나씩 빠져나가 동작
 	private final String THREAD_NAME = "PRJB-THREAD-";		//생성되는 Thread 접두사 지정
